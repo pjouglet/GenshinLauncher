49da3132a5977bdbafbf1a609525a1f992c73141
Pierre <pierre@aliengen.com>
2022-11-28 15:39:13 +0800

    Added more translations

--- PU_Test/Resources/Strings.Designer.cs
+++ PU_Test/Resources/Strings.Designer.cs
@@ -70,11 +70,29 @@ namespace Launcher.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to 取消.
+        /// </summary>
+        public static string CANCEL {
+            get {
+                return ResourceManager.GetString("CANCEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to 配置项不正确！.
         /// </summary>
         public static string CONFIGURATION_ERROR {
             get {
                 return ResourceManager.GetString("CONFIGURATION_ERROR", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 确认.
+        /// </summary>
+        public static string CONFIRM {
+            get {
+                return ResourceManager.GetString("CONFIRM", resourceCulture);
             }
         }
         
@@ -120,6 +138,15 @@ namespace Launcher.Resources {
         public static string LAUNCH {
             get {
                 return ResourceManager.GetString("LAUNCH", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 本地代理端口.
+        /// </summary>
+        public static string LOCAL_PROXY_PORT {
+            get {
+                return ResourceManager.GetString("LOCAL_PROXY_PORT", resourceCulture);
             }
         }
         
@@ -259,6 +286,15 @@ namespace Launcher.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to 服务器地址.
+        /// </summary>
+        public static string SERVER_ADDRESS {
+            get {
+                return ResourceManager.GetString("SERVER_ADDRESS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to OFFICIAL.
         /// </summary>
         public static string SERVER_OFFICIAL {
@@ -273,6 +309,87 @@ namespace Launcher.Resources {
         public static string SERVER_PRIVATE {
             get {
                 return ResourceManager.GetString("SERVER_PRIVATE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 自动搜索.
+        /// </summary>
+        public static string SETTING_AUTOMATIC_LOCATE {
+            get {
+                return ResourceManager.GetString("SETTING_AUTOMATIC_LOCATE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 手动指定位置.
+        /// </summary>
+        public static string SETTING_MANUAL_LOCATE {
+            get {
+                return ResourceManager.GetString("SETTING_MANUAL_LOCATE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 当前游戏路径.
+        /// </summary>
+        public static string SETTINGS_CURRENT_GAME_PATH {
+            get {
+                return ResourceManager.GetString("SETTINGS_CURRENT_GAME_PATH", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 游戏资源.
+        /// </summary>
+        public static string SETTINGS_GAME_RESOURCES {
+            get {
+                return ResourceManager.GetString("SETTINGS_GAME_RESOURCES", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 通用设置.
+        /// </summary>
+        public static string SETTINGS_GENERAL {
+            get {
+                return ResourceManager.GetString("SETTINGS_GENERAL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 本地文件.
+        /// </summary>
+        public static string SETTINGS_LOCAL_FILES {
+            get {
+                return ResourceManager.GetString("SETTINGS_LOCAL_FILES", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 代理.
+        /// </summary>
+        public static string SETTINGS_NETWORK {
+            get {
+                return ResourceManager.GetString("SETTINGS_NETWORK", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 设置.
+        /// </summary>
+        public static string SETTINGS_TITLE {
+            get {
+                return ResourceManager.GetString("SETTINGS_TITLE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 版本信息.
+        /// </summary>
+        public static string SETTINGS_VERSION_INFORMATION {
+            get {
+                return ResourceManager.GetString("SETTINGS_VERSION_INFORMATION", resourceCulture);
             }
         }
         
@@ -300,6 +417,24 @@ namespace Launcher.Resources {
         public static string UNKNOWN_CLIENT_VERSION {
             get {
                 return ResourceManager.GetString("UNKNOWN_CLIENT_VERSION", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 使用http.
+        /// </summary>
+        public static string USE_HTTP {
+            get {
+                return ResourceManager.GetString("USE_HTTP", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 仅启用代理.
+        /// </summary>
+        public static string USE_ONLY_PROXY {
+            get {
+                return ResourceManager.GetString("USE_ONLY_PROXY", resourceCulture);
             }
         }
         
